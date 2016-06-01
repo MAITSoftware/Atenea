05351d10af425f3b8a338cb5fbb271161e3598f8
Mariana Bentancor <mariana.4bv@gmail.com>
2016-06-01 03:20:10 -0300

    Base de datos by Mariana

--- atenea.sql
+++ atenea.sql
@@ -1,18 +1,114 @@
-create database `Atenea`;
-use `Atenea`;
-create table `usuarios` (
-	`ci` VARCHAR(8) NOT NULL PRIMARY KEY,
-    `nombre` VARCHAR(10) NOT NULL,
-    `apellido` VARCHAR(10) NOT NULL,
-    `contraseña` VARCHAR(20) NOT NULL,
-    `tipo` VARCHAR(11) NOT NULL
-);
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: atenea_db
+-- ------------------------------------------------------
+-- Server version	5.7.12-log
 
-create table `libros` (
-	`título` VARCHAR(20) NOT NULL,
-    `autor` VARCHAR(20) NOT NULL default '',
-    `portada` LONGBLOB,
-    `categoría` INT default 0,
-    `id` VARCHAR(20) PRIMARY KEY NOT NULL);
-    
-    
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `libro` (
+  `Titulo` varchar(45) NOT NULL,
+  `Autor` varchar(50) NOT NULL,
+  `Portada` longblob,
+  `Genero` varchar(25) DEFAULT NULL,
+  `Condicion` set('Buen estado','Regular','Mal estado') NOT NULL,
+  `ID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestamo`
+--
+
+DROP TABLE IF EXISTS `prestamo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prestamo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `CI usuario` int(8) DEFAULT NULL,
+  `CI funcionario` int(8) DEFAULT NULL,
+  `Fecha prestamo` date NOT NULL,
+  `Fecha entrega` date DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `CI-usuario-prestamo_idx` (`CI usuario`),
+  KEY `CI-funcionario-prestamo_idx` (`CI funcionario`),
+  CONSTRAINT `CI-funcionario-prestamo` FOREIGN KEY (`CI funcionario`) REFERENCES `usuario` (`CI`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `CI-usuario-prestamo` FOREIGN KEY (`CI usuario`) REFERENCES `usuario` (`CI`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestamo`
+--
+
+LOCK TABLES `prestamo` WRITE;
+/*!40000 ALTER TABLE `prestamo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestamo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `CI` int(8) NOT NULL,
+  `Nombre` varchar(25) DEFAULT NULL,
+  `Apellido` varchar(30) DEFAULT NULL,
+  `Contrasenia` varchar(45) DEFAULT NULL,
+  `Tipo` set('Usuario','Funcionario') NOT NULL,
+  PRIMARY KEY (`CI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'atenea_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-31 20:49:38
